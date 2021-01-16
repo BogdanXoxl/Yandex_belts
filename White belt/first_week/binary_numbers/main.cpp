a265e6810c0af6839bbc4f6e56f0bb9554041801
BogdanXoxl <68805632+BogdanXoxl@users.noreply.github.com>
2021-01-16 17:38:23 +0300

    Add binary numbers and gcd

---
+++ White belt/first_week/binary_numbers/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+int main(){
+    int x = 0, s;
+    vector<int> v;
+    cin>>x;
+
+    while (x >= 1) {
+        v.push_back(x % 2);
+        x = x / 2;
+    }
+
+
+    for_each(v.rbegin(), v.rend(), [](int x){cout<<x;});
+
+    return 0;
+}
--- White belt/first_week/second_occurrence/main.cpp
+++ White belt/first_week/second_occurrence/main.cpp
@@ -1,22 +1,63 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include "../../../Tools/profile.h"
 
 using namespace std;
+//SECOND faster then FIRST  but FIRST more understandable
+
+void first(const string& str){
+    switch (count(str.begin(), str.end(), 'f')) {
+            case 1:
+                cout<<-1<<endl;
+                break;
+            case 0:
+                cout<<-2<<endl;
+                break;
+            default:
+                cout<<str.find('f' , str.find('f')+1)<<endl;
+    }
+}
+
+void second(const string& str){
+    int result = -2;
+
+    for (int i = 0; i < str.size(); ++i)
+     if (str[i] == 'f')
+        if (result == -2)
+            result = -1;
+        else if (result == -1) {
+            result = i;
+            break;
+        }
+    cout << result;
+}
 
 int main(){
-    string str;
-    cin>>str;
 
-    switch (count(str.begin(), str.end(), 'f')) {
-        case 1:
-            cout<<-1<<endl;
-            break;
-        case 0:
-            cout<<-2<<endl;
-            break;
-        default:
-            cout<<str.find('f' , str.find('f')+1)<<endl;
+    {
+        LOG_DURATION("FIRST");
+        first("tuytuystdydasytyutduiydoaisudoiusoiduaoisduaoisduasodyftwyuudoiwuywyustdyuiwoiwudiouwofiusdywtusyduuwoiu");
+    }
+    {
+        LOG_DURATION("SECOND");
+        second("tuytuystdydasytyutduiydoaisudoiusoiduaoisduaoisduasodyftwyuudoiwuywyustdyuiwoiwudiouwofiusdywtusyduuwoiu");
+    }
+    {
+        LOG_DURATION("FIRST");
+        first("ff");
+    }
+    {
+        LOG_DURATION("SECOND");
+        second("ff");
+    }
+    {
+        LOG_DURATION("FIRST");
+        first("fasf");
+    }
+    {
+        LOG_DURATION("SECOND");
+        second("fasf");
     }
     return 0;
 }
