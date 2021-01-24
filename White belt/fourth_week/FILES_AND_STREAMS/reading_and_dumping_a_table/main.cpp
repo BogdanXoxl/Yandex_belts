66f63bbe85359249ca354ae42135c4162377bc72
BogdanXoxl <68805632+BogdanXoxl@users.noreply.github.com>
2021-01-24 19:51:10 +0300

    Add task "reading and dumping a table"

--- White belt/fourth_week/FILES_AND_STREAMS/reading_and_dumping_a_table/main.cpp
+++ White belt/fourth_week/FILES_AND_STREAMS/reading_and_dumping_a_table/main.cpp
@@ -1,4 +1,29 @@
-//
-// Created by Богдан Хохлов on 24.01.2021.
-//
+#include <iostream>
+#include <iomanip>
+#include <fstream>
 
+#include <sstream>
+
+using namespace std;
+
+int main(){
+    ifstream input("input.txt");
+//    stringstream input;
+//    input<<2<<" "<<3<<endl<<1<<","<<2<<","<<3<<endl<<4<<","<<5<<","<<6<<endl;
+    int N, M, data;
+    if(input){
+        input>>N>>M;
+        for(int i = 0; i<N; ++i){
+            for(int j = 0; j<M; ++j){
+                input>>data;
+                input.ignore(1);
+                cout<<setw(10)<<data;
+                if(j+1<M)
+                    cout<<" ";
+            }
+            if(i+1<N)
+                    cout<<endl;
+        }
+    }
+    return 0;
+}
