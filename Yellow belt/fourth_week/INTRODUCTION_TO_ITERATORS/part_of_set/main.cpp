b53fbda6fb028cd548886fb274476418ec8b24ec
BogdanXoxl <68805632+BogdanXoxl@users.noreply.github.com>
2021-02-28 12:55:50 +0300

    Edit task part of set"

--- Yellow belt/fourth_week/INTRODUCTION_TO_ITERATORS/part_of_set/main.cpp
+++ Yellow belt/fourth_week/INTRODUCTION_TO_ITERATORS/part_of_set/main.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 template <typename T>
 vector<T> FindGreaterElements(const set<T>& elements, const T& border){
-    return {find_if(elements.begin(), elements.end(), [&border](const T& i){return i>border;}), elements.end()};
+    //return {find_if(elements.begin(), elements.end(), [&border](const T& i){return i>border;}), elements.end()};
+    return {elements.upper_bound(border), elements.end()};
 }
 
 
